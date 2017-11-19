14c443b28f90e8739344e7580b2cc5009cb1a029
RingoKid <rifat.naimul@gmail.com>
2017-11-19 23:13:46 +0600

    USER extended

--- project/finance/admin.py
+++ project/finance/admin.py
@@ -5,3 +5,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Land)
+admin.site.register(Farmer)
+admin.site.register(Investor)
--- project/finance/models.py
+++ project/finance/models.py
@@ -1,19 +1,25 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 class Farmer(models.Model):
+    user         = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name    = models.CharField(max_length=120)
     age          = models.IntegerField()
-    gender       = models.CharField(max_length=6)
-    address      = models.CharField(max_length=120)
+    # gender       = models.CharField(max_length=6)
+    # address      = models.CharField(max_length=120)
 
     def __str__(self):
         return self.full_name
 
 
+
 class Investor(models.Model):
+    user         = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name    = models.CharField(max_length=120)
     age          = models.IntegerField()
     gender       = models.CharField(max_length=5)
@@ -21,7 +27,6 @@ class Investor(models.Model):
 
     def __str__(self):
         return self.full_name
-
 
 class Land(models.Model):
     location        = models.CharField(max_length=120)
